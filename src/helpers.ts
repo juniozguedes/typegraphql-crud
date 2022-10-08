38917e89246bc07b5af7c1c871bc78a072654220
Wellington <juniozguedes@gmail.com>
2022-10-08 00:08:27 -0300

    Added helpers

---
+++ src/helpers.ts
@@ -0,0 +1,25 @@
+import { Recipe, RecipeModel } from "./recipes/recipe.model";
+import { User, UserModel } from "./users/user.model"
+
+export async function seedDatabase() {
+  const defaultUser = new UserModel({
+    email: "test@github.com",
+    nickname: "MichalLytek",
+    password: "s3cr3tp4ssw0rd",
+  } as unknown as User);
+  await defaultUser.save();
+
+  await RecipeModel.create([
+          {
+              title: "Recipe 1",
+              description: "Desc 1",
+              author: defaultUser._id,
+          },
+          {
+              title: "Recipe 2",
+              author: defaultUser._id
+          },
+      ] as unknown as Recipe[]);
+
+  return { defaultUser };
+}
