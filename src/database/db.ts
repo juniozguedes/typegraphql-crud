3065e4e251bd64d28b0bf1cb6d0b70342db738ae
Wellington <juniozguedes@gmail.com>
2022-10-11 22:47:12 -0300

    Added test logic to db

--- src/database/db.ts
+++ src/database/db.ts
@@ -6,14 +6,23 @@ const logger = pino();
 
 dotenv.config();
 
-export async function connectDb() {
+export async function connectDb(environment) {
   logger.info('Connecting to Mongo Database');
+
+  if (environment == 'test') {
+    const MONGO_DB_URL = process.env.MONGO_TEST_URL;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const mongoose = await connect(MONGO_DB_URL);
+    logger.info('Database connected');
+    return mongoose;
+  }
+
   const MONGO_DB_URL = process.env.MONGO_URL;
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const mongoose = await connect(MONGO_DB_URL);
   logger.info('Database connected');
-
+  return mongoose;
   //logger.info('Cleaning and seeding database');
   // clean and seed database with some data
   //await mongoose.connection.db.dropDatabase();
